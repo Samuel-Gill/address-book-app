8410f3bfbfd90c66bafbdf9f25dd8ac4cd905399
samuelgill1996 <samuelgill1996@gmail.com>
2021-11-04 16:52:19 -0700

    user cards display

---
+++ task0/src/Card.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Card(props) {
+    return (
+        <>
+            <div className="cards">
+                <div className="card">
+                    <img className="card_img" src="https://picsum.photos/200/100" alt="userimg" />
+
+                    <div className="card_info">
+                        <h5 className="card_title"> {props.name} </h5>
+                        <p className="card_text"> {props.email} </p>
+                        <a href="/" target="_blank">
+                            <button>Go Home</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card;
---
+++ task0/src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+        id: 1,
+        name: "Samuel Gill",
+        email: "samuel@mail.com",
+    },
+    {
+        id: 2,
+        name: "James Gill",
+        email: "james@mail.com",
+    },
+    {
+        id: 3,
+        name: "Mona Liza",
+        email: "mona@mail.com",
+    },
+    {
+        id: 4,
+        name: "Steve Jobs",
+        email: "steve@mail.com",
+    },
+    {
+        id: 5,
+        name: "Angelina Jolie",
+        email: "angelina@mail.com",
+    },
+    {
+        id: 6,
+        name: "Indiana Jones",
+        email: "indiana@mail.com",
+    },
+];
+
+export default Sdata;
