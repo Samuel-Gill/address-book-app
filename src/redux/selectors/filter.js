60ae826ea4fbde4c3854ea3e09916d89c4726462
samuelgill1996 <samuelgill1996@gmail.com>
2021-12-17 19:12:24 -0800

    adding fetchMoreSelector

--- src/redux/selectors/filter.js
+++ src/redux/selectors/filter.js
@@ -5,5 +5,17 @@ export const filterSelector = createSelector(
     (_, filtered) => filtered,
     (_, fil, userLimit) => userLimit,
     (data, filtered, userLimit) =>
-        data.users.filter((item) => `${item.name.title} ${item.name.first} ${item.name.last}`.toLowerCase().includes(filtered)).slice(userLimit)
+        data.users.filter((item) => `${item.name.title} ${item.name.first} ${item.name.last}`.toLowerCase().includes(filtered)).slice(0, userLimit)
+)
+
+export const fetchMoreSelector = createSelector(
+    (state) => state.user,
+    (data) => {
+        if (data.users.length < 1000) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 )
